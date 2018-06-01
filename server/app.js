3b1247567dea7985f94bb7a49ed6b666089881f6
igor atakhanov <igoratakhanov@gmail.com>
2018-06-01 17:43:09 -0400

    article posting, backend and front end working

--- server/app.js
+++ server/app.js
@@ -1,79 +1,71 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const cors = require('cors')
-const errorHandler = require('errorhandler')
-const mongoose = require('mongoose')
 
-// Replace mongoose's native promise library with native
-mongoose.promise = global.Promise
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
 
-const isProduction = process.env.NODE_ENV === 'production'
+mongoose.promise = global.Promise;
 
-const app = express()
+const isProduction = process.env.NODE_ENV === 'production';
 
-app.use(cors())
-app.use(require('morgan')('dev'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({ 
-    secret: 'LightBlog', 
-    cookie: { maxAge: 60000 }, 
-    resave: false, 
-    saveUninitialized: false 
-}))
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+  next()
+})
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'LightBlog', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if(!isProduction) {
-    app.use(errorHandler())
+  app.use(errorHandler());
 }
 
-mongoose.connect('mongodb://localhost/lightblog')
-mongoose.set('debug', true)
+mongoose.connect('mongodb://igor:password1@ds245210.mlab.com:45210/simple_blog_dev');
+mongoose.set('debug', true);
+
+// Add models
+require('./models/Articles');
+// Add routes
+app.use(require('./routes'));
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found')
-    err.status = 404
-    next(err)
-})
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 if (!isProduction) {
-    app.use((err, req, res) => {
-        res.status(err.status || 500)
-    
-        res.json({
-            errors: {
-                message: err.message,
-                error: err,
-            },
-        })
-    })
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
 }
 
-  mongoose.connect('mongodb://localhost/lightblog');
-  mongoose.set('debug', true);
-  
-  // Add models
-  require('./models/Articles');
-  // Add routes
-  
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
 app.use((err, req, res) => {
-    res.status(err.status || 500);
-  
-    res.json({
-        errors: {
-            message: err.message,
-            error: {},
-        },
-    });
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
-  
+
 const server = app.listen(8000, () => console.log('Server started on http://localhost:8000'));
--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
-const router = require('express').Router()
-const Articles = mongoose.model('Articles')
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const Articles = mongoose.model('Articles');
 
 router.post('/', (req, res, next) => {
-  const { body } = req
+  const { body } = req;
 
   if(!body.title) {
     return res.status(422).json({
       errors: {
         title: 'is required',
       },
-    })
+    });
   }
 
   if(!body.author) {
@@ -18,7 +18,7 @@ router.post('/', (req, res, next) => {
       errors: {
         author: 'is required',
       },
-    })
+    });
   }
 
   if(!body.body) {
@@ -26,63 +26,62 @@ router.post('/', (req, res, next) => {
       errors: {
         body: 'is required',
       },
-    })
+    });
   }
 
-const finalArticle = new Articles(body)
-return finalArticle.save()
+  const finalArticle = new Articles(body);
+  return finalArticle.save()
     .then(() => res.json({ article: finalArticle.toJSON() }))
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.get('/', (req, res, next) => {
   return Articles.find()
     .sort({ createdAt: 'descending' })
     .then((articles) => res.json({ articles: articles.map(article => article.toJSON()) }))
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.param('id', (req, res, next, id) => {
   return Articles.findById(id, (err, article) => {
     if(err) {
-      return res.sendStatus(404)
+      return res.sendStatus(404);
     } else if(article) {
-      req.article = article
-      return next()
+      req.article = article;
+      return next();
     }
-  }).catch(next)
-})
+  }).catch(next);
+});
 
 router.get('/:id', (req, res, next) => {
   return res.json({
     article: req.article.toJSON(),
-  })
-})
+  });
+});
 
 router.patch('/:id', (req, res, next) => {
-  const { body } = req
+  const { body } = req;
 
   if(typeof body.title !== 'undefined') {
-    req.article.title = body.title
+    req.article.title = body.title;
   }
 
   if(typeof body.author !== 'undefined') {
-    req.article.author = body.author
+    req.article.author = body.author;
   }
 
   if(typeof body.body !== 'undefined') {
-    req.article.body = body.body
+    req.article.body = body.body;
   }
 
   return req.article.save()
     .then(() => res.json({ article: req.article.toJSON() }))
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.delete('/:id', (req, res, next) => {
   return Articles.findByIdAndRemove(req.article._id)
     .then(() => res.sendStatus(200))
-    .catch(next)
-})
-
-module.exports = router
+    .catch(next);
+});
+module.exports = router;
