4dec5a7c152232d890cf3a9ed5331a8a41eba95c
igor atakhanov <igoratakhanov@gmail.com>
2018-06-02 16:31:12 -0400

    All done: redux, editing, backend implemented

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-
-import { Form } from '../../components/Article';
+import React from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import moment from "moment";
+import { Form } from "../../components/Article";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  componentDidMount() {
+    const { onLoad } = this.props;
+    axios("http://localhost:8000/api/articles").then(res => onLoad(res.data));
+  }
+
+  handleDelete(id) {
+    const { onDelete } = this.props;
+    // test this to see if backend delete fail results in front end delete success
+    return axios
+      .delete(`http://localhost:8000/api/articles/${id}`)
+      .then(() => onDelete(id));
+  }
+
+  handleEdit(article) {
+    const { setEdit } = this.props;
+
+    setEdit(article);
+  }
+
   render() {
+    console.log("home's props:", this.props);
+
+    const { articles } = this.props;
     return (
       <div className="container">
         <div className="row pt-5">
@@ -12,10 +43,55 @@ class Home extends React.Component {
           </div>
           <Form />
         </div>
+
+        <div className="row pt-5">
+          <div className="col-12 col-lg-6 offset-lg-3">
+            {articles.map(article => {
+              return (
+                <div key={article._id} className="card my-3">
+                  <div className="card-header">{article.title}</div>
+                  <div className="card-body">
+                    {article.body}
+                    <p className="mt-5 text-muted">
+                      <b>{article.author}</b>{" "}
+                      {moment(new Date(article.createdAt)).fromNow()}
+                    </p>
+                  </div>
+
+                  <div className="card-footer">
+                    <div className="row">
+                      <button
+                        onClick={() => this.handleEdit(article)}
+                        className="btn btn-primary mx-3"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => this.handleDelete(article._id)}
+                        className="btn btn-danger"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  articles: state.home.articles
+});
 
+const mapDispatchToProps = dispatch => ({
+  onLoad: data => dispatch({ type: "HOME_PAGE_LOADED", data }),
+  onDelete: id => dispatch({ type: "DELETE_ARTICLE", id }),
+  setEdit: article => dispatch({ type: 'SET_EDIT', article }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import ReactDOM from 'react-dom';
 import { Route, Switch, Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import store from './store';
 import { App } from './components';
+console.log("frontend process env", process.env.NODE_ENV)
 
 import '../resources/scss/style.scss';
 
 ReactDOM.render(
   <Router history={createHistory()}>
-    <Switch>
-      <Route path="/" component={App} />
-    </Switch>
+    <Provider store={store} >
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Provider>
   </Router>,
   document.getElementById('root'),
 );
